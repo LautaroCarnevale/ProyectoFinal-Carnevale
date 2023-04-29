abc876694fc34d099376d0ab9012e99d5c6728d5
lautaro <carnevalelautaro72@gmail.com>
2023-04-29 20:43:25 -0300

    Registro de camiones en un puerto

--- app.js
+++ app.js
@@ -1,101 +1,143 @@
-alert("Bienvenido al mercado, primero necesito que crees una cuenta.");
+// alert("Bienvenido al Banco, primero necesito que crees una cuenta.");
 
-let usuario;
-let contraseña;
+// let usuario;
+// let contraseña;
 
 
-// Crer una cuenta
-usuario = prompt("ingrese un usuario");
-//Inicio de sesion
-if (usuario != "") {
-    contraseña = prompt("ingrese una contraseña");
-    if (contraseña != "") {
-        alert('Ingresaste los datos de registro correctamente, ahora ingrese a su cuenta, iniciando sesión.');
+// // Crer una cuenta
+// usuario = prompt("ingrese un usuario");
+// //Inicio de sesion
+// if (usuario != "") {
+//     contraseña = prompt("ingrese una contraseña");
+//     if (contraseña != "") {
+//         alert('Ingresaste los datos de registro correctamente, ahora ingrese a su cuenta, iniciando sesión.');
 
-        if (usuario == prompt("Ingrese su usuario")) {
-            alert("Tu usuario es correcto, ahora ingresa la contraseña.");
+//         if (usuario == prompt("Ingrese su usuario")) {
+//             alert("Tu usuario es correcto, ahora ingresa la contraseña.");
 
-            if (contraseña == prompt("ingrese su contraseña")) {
-                alert("El usuario y contraseña son correctos, bienvenido al mercado!");
+//             if (contraseña == prompt("ingrese su contraseña")) {
+//                 alert("El usuario y contraseña son correctos!");
 
-                //mercado
-                alert('Elige productos que deseas comprar y te diremos cuanto es el total...' + '\n' +
-                    'Manzana: $50' + '\n' +
-                    'Pollo: $450' + '\n' +
-                    'Pan: $100' + '\n' +
-                    'Queso: $150' + '\n' +
-                    'Arroz: $90' + '\n' +
-                    'Atun: $100' + '\n' +
-                    'Papel: $110');
-                //productos
-                let manzana = 50;
-                let pollo = 450;
-                let pan = 100;
-                let queso = 150;
-                let arroz = 90;
-                let atun = 100;
-                let papel = 110;
-                let total = 0;
-                let resultado = 0;
-                let seguirComprando;
-                let continuar = 's';
+//contenido
 
-                while (seguirComprando != 'n') {
-                    let totalProducto = Number(prompt("¿cuantos prductos deseas comprar? el maximo es 7"));
-                    if (totalProducto <= 7) {
-                        for (let i = 1; i <= totalProducto; i++) {
-                            let producto = prompt('Ingrese sus productos' + '\n' +
-                            'Manzana: $50' + '\n' +
-                            'Pollo: $450' + '\n' +
-                            'Pan: $100' + '\n' +
-                            'Queso: $150' + '\n' +
-                            'Arroz: $90' + '\n' +
-                            'Atun: $100' + '\n' +
-                            'Papel: $110');
-                            switch (producto) {
-                                case 'manzana':
-                                    total = total + manzana;
-                                    break;
-                                case 'pollo':
-                                    total = total + pollo;
-                                    break;
-                                case 'pan':
-                                    total = total + pan;
-                                    break;
-                                case 'queso':
-                                    total = total + queso;
-                                    break;
-                                case 'arroz':
-                                    total = total + arroz;
-                                    break;
-                                case 'atun':
-                                    total = total + atun;
-                                    break;
-                                case 'papel':
-                                    total = total + papel;
-                                    break;
-                                default:
+//             } else {
+//                 alert("La es contraseña es incorrecta.");
+//             }
+//         } else {
+//             alert("El usuario es incorrecto.");
+//         }
+//     } else {
+//         alert("No ingresaste una contraseña, lamentablemente no vamos a poder registrarte.");
+//     }
+// } else {
+//     alert("No ingresaste un usuario, lamentablemente no vamos a poder registrarte.");
+// }
+
+
+
+
+/* 
+1-  llega el camion al puerto.
+2- saca un turno 
+3- va al la casilla a registrar su camion(le piden el turno si no es correcto no entra)
+4- le piden todos los datos del camion y del conductor: 1. se agrega automaticamente su numero de turno.
+                            2. Datos de destinatario y destino.
+                            3. Datos del chofer(nombre completo, dni,  cuit, nacionalidad) .
+                            4. Fecha de hoy.
+                            5. modelo del camion.
+                            6. patente del camion.
+                            7. Grano/especie(si tiene humedad, vichos plagucida fumigante).
+                            8. Peso neto del grano(kg).
+                        	
+
+
+5- requisitos del camion(con su carga) y conductor: 1. que conincida el destinatario y destino con el registro ya enviado del puerto anteriormente
+                            2. que los datos del chofer sean correctos(vrificar que sea de argentina, que ponga correctamente el cuit y dni).
+                            3. Verificar al fecha de hoy 
+                            4. Verificar modelo del camion.
+                            5. verificar la patente del camion.
+                            6. verificar que el grano/especie sean correctos en ese puesto(si tiene humedad, vichos plagucida fumigante).
+                            7. El peso, si el peso neto es mayor a 30000(kg) no dejarlo descargar el cereal.
+
+6- Si logro registrar su camion en la bases de datos del puerto dejarlo pasar y darle un horario de descarga mas un registro de todos los datos ingresados.
+*/
+
+// function ingresarCamiones () {
+//     const imputUsuario = 
+// }
+
+
+
+
+alert('Esto es una simulación de un registro de un camion y su chofer en un puerto de carga y descarga de cereales(maiz, soja, sorgo,etc)')
+alert('Bievenidos al puerto primero tienes que sacar un turno')
+// turno = prompt('Coloque su nombre y su turno se gurdara')
+
+let turnoActual = 0;
+let entregaTurno = -1;
+let turnoNuevo = false;
+
+do {
+  function crearTurno() {
+    turnoActual++;
+    return turnoActual;
+  }
+
+  let nuevoTurno = crearTurno();
+  nombreTurno = prompt('ingrese un su nombre para registrar su turno..')
+
+  alert(`Su número de turno queda registrado al nombre de ${nombreTurno} y su turno es ${nuevoTurno}.`);
+
+  alert('Bienvenido a la Casilla de Control de Acceso. Por favor, ingrese su número de turno para ingresar. Si el número es correcto, se le permitirá el acceso. De lo contrario, deberá sacar otro turno.');
+  entregaTurno = parseInt(prompt('Ingrese su número de turno...'));
+
+  if (entregaTurno === nuevoTurno) {
+    alert('✅ | Ingreso al registro de camiones correctante, ahora registre su camión. 🚚');
+    turnoNuevo = false;
+
+    const puerto = [];
+
+    let fecha = prompt('Ingrese la fecha de hoy');
+    let nombreCompleto = prompt('Ingrese su nombre completo');
+    let modelo = prompt('Ingrese el modelo de su camion');
+    let patenteLogs = prompt('Ingrese la patente de su camion');
+    let cereal = prompt('Ingrese el grano/cereal');
+    let pesoNeto = Number(prompt('Ingrese el peso neto'));
     
-                                alert('Ingresó un producto que no está registrado en la lista');
-                            }
-                        } 
-    
-                        resultado = total
-                        alert('El total es de: $' + resultado);
-                        seguirComprando = prompt('¿queres seguir comprando? s/n');
-                    } else {
-                        alert('Solo tenmos 7 productos diferentes')
-                    }
-                }
-            } else {
-                alert("La es contraseña es incorrecta.");
-            }
-        } else {
-            alert("El usuario es incorrecto.");
-        }
-    } else {
-        alert("No ingresaste una contraseña, lamentablemente no vamos a poder registrarte.");
-    }
-} else {
-    alert("No ingresaste un usuario, lamentablemente no vamos a poder registrarte.");
-}
+    puerto.push(new camion(nuevoTurno, fecha, nombreCompleto, modelo, patenteLogs, cereal, pesoNeto));
+
+    alert(`           N° de turno: ${nuevoTurno} resgitrado por ${nombreTurno}.
+           Datos del chofer: 
+           ===========================
+           - Nombre completo:${nombreCompleto}.
+           - DNI:
+           - CUIT:
+           - Nacionalidad:
+           ===========================
+           Fecha: ${fecha}.
+           Modelo del camion: ${modelo}.
+           Patente del camion: ${patenteLogs}.
+           Grano/especie: ${cereal}.
+           Peso neto (en kg): ${pesoNeto}.`);
+
+
+  } else {
+    alert('❌ | Su número de turno NO corresponde con el número ya registrado. Por favor, saque otro turno.');
+    turnoNuevo = true;
+  }
+} while (turnoNuevo); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ puerto.js
@@ -0,0 +1,21 @@
+class camion {
+    id;
+    fecha;
+    identificación;
+    modelo;
+    patente;
+    carga;
+    peso;
+
+    constructor (id,fecha,identificación,modelo,patente,carga,peso) {
+        this.id = id;
+        this.fecha = fecha;
+        this.identificación = identificación;
+        this.modelo  = modelo;
+        this.patente = patente;
+        this.carga = carga;
+        this.peso = peso;
+    }
+}
+
+
